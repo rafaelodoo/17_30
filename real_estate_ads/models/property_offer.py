b9c413dc34d1c9bb2b0e4aff2fb02842f9a7f6f7
Rafail <154279905+rafaelodoo@users.noreply.github.com>
2024-12-05 10:07:04 -0600

    3030

--- real_estate_ads/models/property_offer.py
+++ real_estate_ads/models/property_offer.py
@@ -115,7 +115,7 @@ class PropertyOffer(models.Model):
         self.status = 'refused'
         if all(self.property_id.offer_ids.mapped('status')):
             self.property_id.write({
-                'selling_price':0
+                'selling_price':0,
                 'state':'received'
             })
             
