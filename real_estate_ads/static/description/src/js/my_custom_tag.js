a80e45cc58b5296caeb66fc3e555e70f29403255
Rafail <154279905+rafaelodoo@users.noreply.github.com>
2024-12-05 14:11:07 -0600

    41

--- real_estate_ads/static/description/src/js/my_custom_tag.js
+++ real_estate_ads/static/description/src/js/my_custom_tag.js
@@ -3,22 +3,10 @@
 import { registry } from "@web/core/registry";
 import { Component } from "@odoo/owl";
 
-class MyCustomActions extends Components {}
-MyCustomActions.template = "CustomActions"
+class MyCustomActions extends Component {}
+MyCustomAction.template = "CustomActions"
 
-registry.category("actions").add("custom_client_action",MyCustomActions);
+registry.category("actions").add("custom_client_action",MyCustomAction);
 
-// odoo.define('real_estate_ads.CustomAction',function(require){
-//     "use strict";
-//     var AbstractionAction = require('web.Abstraction');
-//     var core = require('web.Core');
 
-//     AbstractionAction.extend({
-//         template: "",
-//         start:function(){
-//             console.log("Action")
-//         }
-//     })
-
-//     core.action_registry.add("custom_client_action",CustomAction)
-// });
+ 
