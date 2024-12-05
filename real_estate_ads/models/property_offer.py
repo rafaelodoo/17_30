4459daa0c65eb2e2d1fc6952413c40ea536ea181
Rafail <154279905+rafaelodoo@users.noreply.github.com>
2024-12-05 10:18:37 -0600

    3030

--- real_estate_ads/models/property_offer.py
+++ real_estate_ads/models/property_offer.py
@@ -116,7 +116,7 @@ class PropertyOffer(models.Model):
         if all(self.property_id.offer_ids.mapped('status')):
             self.property_id.write({
                 'selling_price':0,
-                'state':'refused'
+                'state':'cancel'
             })
             
 
